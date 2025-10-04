266cdb9371533217559b59ccf047abd4a735fdf7
Bobby100 <rjcicc@gmail.com>
2025-10-05 00:24:37 +0200

    Remove 'Add Document' button from sidebar menu

--- src/components/SOSPage.tsx
+++ src/components/SOSPage.tsx
@@ -206,10 +206,6 @@ export default function SOSPage() {
                   </button>
                 ))}
               </nav>
-
-              <button className="w-full mt-4 px-4 py-2 border-2 border-dashed border-gray-300 rounded-lg text-gray-600 hover:border-gray-400 hover:text-gray-700 transition-colors text-sm font-medium">
-                + Add Document
-              </button>
             </div>
           </div>
 
