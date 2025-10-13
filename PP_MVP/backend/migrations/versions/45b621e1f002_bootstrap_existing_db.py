d2cf5686353a260cd56784f637a81ecdd98b1449
Bobby100 <rjcicc@gmail.com>
2025-10-13 07:45:01 -0400

    chore: add alembic bootstrap migration

---
+++ PP_MVP/backend/migrations/versions/45b621e1f002_bootstrap_existing_db.py
@@ -0,0 +1,25 @@
+"""bootstrap_existing_db
+
+Revision ID: 45b621e1f002
+Revises: 
+Create Date: 2025-10-13 07:44:15.935788
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = '45b621e1f002'
+down_revision = None
+branch_labels = None
+depends_on = None
+
+
+def upgrade() -> None:
+    pass
+
+
+def downgrade() -> None:
+    pass
+
